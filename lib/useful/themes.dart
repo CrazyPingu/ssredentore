9489da604674c9f86cd0ab0d4550f05767a0ba5b
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2023-11-18 21:52:49 +0100

    Fix theme color

--- lib/useful/themes.dart
+++ lib/useful/themes.dart
@@ -5,24 +5,40 @@ import 'package:flutter/material.dart';
 class ThemeClass {
   ThemeClass._();
 
-  Color lightPrimaryColor = Colors.deepPurple;
-  Color lightAccentColor = Colors.deepPurpleAccent;
-  Color darkPrimaryColor = Colors.deepOrange;
-  Color darkAccentColor = Colors.deepOrangeAccent;
+  final Color _lightPrimaryColor = const Color.fromARGB(255, 7, 2, 130);
+  final Color _lightBackgroundColor = const Color.fromARGB(255, 196, 188, 204);
+
+  final Color _darkPrimaryColor = Colors.deepOrange;
+  final Color _darkBackgroundColor = const Color.fromARGB(255, 42, 40, 40);
+  // final Color _darkBackgroundColor = Colors.black;
 
   static ThemeData lightTheme = ThemeData(
     primaryColor: ThemeData.light().scaffoldBackgroundColor,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor:
+            MaterialStateProperty.all<Color>(ThemeClass._()._lightPrimaryColor),
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+      ),
+    ),
     colorScheme: const ColorScheme.light().copyWith(
-      primary: ThemeClass._().lightPrimaryColor,
-      secondary: ThemeClass._().lightAccentColor,
+      primary: ThemeClass._()._lightPrimaryColor,
+      background: ThemeClass._()._lightBackgroundColor,
     ),
   );
 
   static ThemeData darkTheme = ThemeData(
     primaryColor: ThemeData.dark().scaffoldBackgroundColor,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor:
+            MaterialStateProperty.all<Color>(ThemeClass._()._darkPrimaryColor),
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+      ),
+    ),
     colorScheme: const ColorScheme.dark().copyWith(
-      primary: ThemeClass._().darkPrimaryColor,
-      secondary: ThemeClass._().darkAccentColor,
+      primary: ThemeClass._()._darkPrimaryColor,
+      background: ThemeClass._()._darkBackgroundColor,
     ),
   );
 }
